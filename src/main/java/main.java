7ef041ab908e144003a23ffb606f94e22ff56688
jbelew <john.belew@gmail.com>
2022-03-06 17:21:46 -0800

    Added stubbed getters

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,84 +1,68 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import utils.TensorVision.Ball;
-import utils.TensorVision.Box;
 
 class SampleApp {
 
-    public static double xMin, yMin, xMax, yMax;
-    public static List<Ball> balls;
+    private static Ball[] balls;
     private static String detectionsEntry;
+    private static String teamColor = "red";
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of("example1.json");
         detectionsEntry = Files.readString(fileName);
 
-        final ObjectMapper objectMapper = new ObjectMapper();
-        Ball[] balls = objectMapper.readValue(detectionsEntry, Ball[].class);
+        balls = parseTargets(detectionsEntry);
 
-        System.out.println(balls[1].color);
-        // System.out.println(getClosestBall(false));
-    }
-
-    private static void readBallData() {
-        // balls.clear(); // refresh balls
-        String output = detectionsEntry;
-
-        ObjectMapper mapper = new ObjectMapper();
-        JsonFactory factory = new JsonFactory();
-
-        try {
-            JsonParser parser = factory.createParser(output);
-            balls = mapper.readTree(parser);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static Ball getClosestBall(boolean red) {
-        String color;
-
-        if (red) {
-            color = new String("red");
+        if (hasTargets(balls, teamColor)) {
+            System.out.println("Found target!");
         } else {
-            color = new String("blue");
+            System.out.println("Not targets found!");
         }
+    }
 
-        readBallData(); // get the list of all balls in our field of view
+    public static Ball[] parseTargets(String payload) throws JsonParseException, JsonMappingException, IOException {
+        // balls.clear(); // refresh balls
+        String detections = payload;
 
-        int closestBallIndex = 0;
+        final ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(detections, Ball[].class);
+    }
 
-        // delete balls from list that aren't of our color
-        for (int i = 0; i < balls.size(); i++) {
-            if (balls.get(i).color != color) {
-                balls.remove(i);
-                i--; // we have deleted an element, so our index must shrink by 1
+    // Do we have a target, if not, move on ...
+    public static boolean hasTargets(Ball[] ballList, String teamColor) {
+        boolean foundTarget = false;
+        for (int i = 0; i < ballList.length; i++) {
+            System.out.println(ballList[i].color);
+            if (ballList[i].color.equals(teamColor)) {
+                foundTarget = true;
             }
         }
+        return foundTarget;
+    }
 
-        // get index of ball with largest box
-        for (int i = 0; i < balls.size(); i++) {
-            Box currentBox = balls.get(i).box;
+    // Get the array index of the best target.
+    public static int getBestTarget(Ball[] ballList, String teamColor) {
+        int bestTarget = 0;
+        return bestTarget;
+    }
 
-            Box largestBox = balls.get(closestBallIndex).box;
+    // Used for setting strength controller rumble
+    public static int getBestTargetDistance() {
+        int targetDistance = 0;
+        return targetDistance;
+    }
 
-            if (currentBox.xMax - currentBox.xMin > largestBox.xMax - largestBox.xMin) {
-                closestBallIndex = i;
-            }
-        }
-
-        return balls.get(closestBallIndex);
+    // Get the Yaw of the best target to turn the robot
+    public static double getBestTargetYaw() {
+        int targetYaw = 0;
+        return targetYaw;
     }
 
 }
